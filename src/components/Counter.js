8ec8d78adec82c1a229d6a90798c0e98a6ec0fa3
elkhayate <melkhayate61@gmail.com>
2021-05-22 09:12:14 +0300

    fixed Start Stop with state changing

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,44 +1,64 @@
-import React,{useRef, useState} from 'react';
-import Countdown,{zeroPad} from "react-countdown";
+import React, { useRef, useState, useMemo } from "react";
+import Countdown, { zeroPad } from "react-countdown";
 import style from "./counter.module.css";
 
-
-
-const renderer = ({minutes, seconds}) => {
-      return <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>
-    
-  };
-  
+const renderer = ({ minutes, seconds }) => {
+  return (
+    <span>
+      {zeroPad(minutes)}:{zeroPad(seconds)}
+    </span>
+  );
+};
 
 function Counter() {
-    const [pause, setPause] = useState(true)
-    const clockRef = useRef();
-    const handleStart = () => {
-        clockRef.current.start();
-    }
-    const handlePause = (e) => {
-        clockRef.current.pause();
-        
-    }
-  
-    return (
-        <div className={style.counter}>
-            <div className={style.items}>
-                <div>Pomodoro</div>
-                <div>Short Break</div>
-                <div>Long Break</div>
-            </div>
-        <h1 className={style.count}><Countdown
-            date={Date.now() + 1200000}
-            ref={clockRef}
-            autoStart={false}
-            renderer = {renderer}
-        /></h1>
-         <button className={style.btn} onClick={handleStart}>START</button> 
-         <button className={style.btn} onClick={handlePause}>PAUSE</button>
-        
-        </div>
-    );
+  const date = useMemo(()=> {return (Date.now() + 1200000)}, []);
+  const [pause, setPause] = useState(true);
+  const clockRef = useRef();
+  const handleStart = () => {
+    clockRef.current.start();
+  };
+  const handlePause = () => {
+    clockRef.current.pause();
+  };
+  const Pause = () => {
+    setPause(!pause);
+  };
+
+  return (
+    <div className={style.counter}>
+      <div className={style.items}>
+        <div>Pomodoro</div>
+        <div>Short Break</div>
+        <div>Long Break</div>
+      </div>
+      <h1 className={style.count}>
+        <Countdown
+          date={date}
+          ref={clockRef}
+          autoStart={false}
+          renderer={renderer}
+        />
+      </h1>
+      <button
+        className={style.btn}
+        onClick={() => {
+          handleStart();
+          Pause();
+        }}
+      >
+        START
+      </button>
+      <button
+        className={style.btn}
+        onClick={() => {
+          handlePause();
+          Pause();
+        }}
+      >
+        PAUSE
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
